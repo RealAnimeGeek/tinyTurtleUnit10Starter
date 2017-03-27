f620bd947d9d6035ad27e01782ad311ec8f09a55
Abigail deMontrichard <abbiedemont@gmail.com>
2017-03-27 20:14:17 +0000

    shapes

--- script.js
+++ script.js
@@ -11,8 +11,46 @@ forward(50);
 right(90);
 forward(50);
 }
+function triangle(){
+right(30);
+forward(50);
+right(120);
+forward(50);
+right(120);
+forward(50);
+}  
 
+function rectangle(){
+forward(40);
+right(90);
+forward(100);
+right(90);
+forward(50);
+right(90);
+forward(100);
+right(90);
+forward(50);
+}
+function pentagon(){
+left(72);
+forward(50);
+left(72);
+forward(50);
+left(72);
+forward(50);
+left(72);
+forward(50);
+left(72);
+forward(50);
+}
+function house(){
+square();
+forward(1);
+triangle();
+}
 // Type your function call below
 
+//triangle();
+house();
 
 stamp();
